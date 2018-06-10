554cbfa3621252282d236b3df37d12bbc1f3354b
Adnan RIHAN <adnan@rihan.fr>
2018-06-11 00:48:20 +0100

    Added an example project: Find Available Addresses

--- TankRange_I2C_UNO/TankRange_I2C_UNO.ino
+++ TankRange_I2C_UNO/TankRange_I2C_UNO.ino
@@ -1,6 +1,6 @@
 /*
 |--------------------------------------------------------------------------
-| Maxbotix Hardware I2C
+| Maxbotix Hardware I2C Example
 |--------------------------------------------------------------------------
 |
 | Author: Adnan RIHAN
@@ -9,320 +9,68 @@
 |
 */
 
-#include <Wire.h>
+#include    "Maxbotix.h"
 
-// Uncomment this line to show debug
-// #define SERIAL_DEBUG
-
-// These are the default I2C pins on Arduino UNO
-#define SDA_PIN     A4
-#define SCL_PIN     A5
-
-#define I2C_TIMEOUT     100
-#define RETRY_TIMEOUT   10000
-
-// The default I2C address for Maxbotix sonars
-// is 224 on 8 bits, 112 on 7bits.
-// Arduino I2C library currently uses 7bit addresses
-#define MB_ADDR     112
-#define MB_READ_CMD byte(81)
+int         range;
+Maxbotix    sonar;
 
 void    setup()
 {
-    serialInit(57600);
-
-    pinMode(SDA_PIN, INPUT_PULLUP);
-    pinMode(SCL_PIN, INPUT_PULLUP);
-
-// Force the useless LED to stay off
+// Force the useless LED to stay off with some clones
 #ifdef LED_BUILTIN
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 #endif
 
-    serialPrint("* Joining I2C bus as master... ");
-    Wire.begin();
-    serialPrintln("Done.\n");
+    Serial.begin(57600);
+    while (!Serial);
+    Serial.println("Serial ready.\n");
 }
 
 void    loop()
 {
-    int    range;
+    Serial.print("Probing: ");
+    range = sonar.probe();
 
-    do {
-        range = ping(MB_ADDR);
+    // Range < 0 means there is an I2C error
+    if (range < 0) {
+        Serial.print("error(");
+        Serial.print(range, DEC);
+        Serial.println(")");
+    } else {
+        Serial.print(range, DEC);
+        Serial.println("cm");
+    }
 
-        if (range < 0) {
-            serialPrintln("* Looping in 10 seconds.");
-            delay(RETRY_TIMEOUT);
-        }
-    } while (1);
+    delay(2000);
 }
 
-void    serialInit(unsigned long baud)
+void    scanAddresses(void)
 {
-#ifdef SERIAL_DEBUG
-    Serial.begin(baud);
-    while (!Serial);
-    Serial.println("*Serial ready.\n");
-#endif
-}
-
-byte    scanAddresses(void)
-{
-    byte    addr = 0;
     byte    error = 0;
 
-    serialPrintln("* Scanning valid addresses:");
+    Serial.println("* Scanning valid addresses:");
 
-    for (byte cur = 8; cur <= 127; cur += 2) {
-        serialPrint("- ");
-        serialPrint(cur);
-        serialPrint(": ");
+    // Will scan all valid I2C addresses
+    // See: http://www.arduino.cc/en/Reference/Wire.html
+    for (byte addr = 8; addr <= 127; addr += 2) {
+        Serial.print("- ");
+        Serial.print(addr);
+        Serial.print(": ");
 
-        Wire.beginTransmission(cur);
-        Wire.write(MB_READ_CMD);
+        Wire.beginTransmission(addr);
+        Wire.write(byte(81));
         error = Wire.endTransmission();
 
         if (error != 0) {
-            serialPrint("Error(");
-            serialPrint(error);
-            serialPrintln(")");
+            Serial.print("Error(");
+            Serial.print(error);
+            Serial.println(")");
 
             continue;
         }
-        serialPrintln(" OK");
-
-        addr |= cur;
+        Serial.println(" OK");
     }
 
-    serialPrintln("* End of Scan\n");
-
-    if (!addr) {
-        serialPrintln("** No valid address found. Retrying in 10 seconds.");
-    }
-
-    return addr;
-}
-
-int    ping(byte addr)
-{
-    byte    error = 0;
-    int     range = 0;
-
-    serialPrint("Sending byte '");
-    serialPrint(MB_READ_CMD);
-    serialPrint("' to #");
-    serialPrint(addr);
-    serialPrint("': ");
-
-    // Sends command byte and capture error
-    Wire.beginTransmission(addr);
-    Wire.write(MB_READ_CMD);
-    error = Wire.endTransmission();
-
-    // In case of an error, hangs for RETRY_TIMEOUT seconds
-    if (error != 0) {
-        serialPrint("Error(");
-        serialPrint(error);
-        serialPrintln(')');
-
-        // To differenciate a range from an error
-        // ping() will return negative error
-        return -error;
-    }
-
-    serialPrintln("OK.");
-    delay(I2C_TIMEOUT);
-
-    serialPrint("Requesting 2 bytes from '");
-    serialPrint(addr);
-    serialPrint(": ");
-    serialPrint(Wire.requestFrom(int(addr), 2));
-    serialPrint(". ");
-
-    while (byte av = Wire.available()) {
-        range = Wire.read() << ((av - 1) * 8);
-    }
-    serialPrint(byte(range));
-    serialPrintln(" cm.\n");
-
-    return range;
-}
-
-// Serial reimplement
-
-size_t  serialPrint(const __FlashStringHelper *data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrint(const String &data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrint(const char data[])
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrint(char data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrint(unsigned char data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(int data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(unsigned int data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(long data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(unsigned long data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(double data, int format = BIN)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data, format);
-#endif
-}
-
-size_t  serialPrint(const Printable &data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrint(const byte data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.print(data);
-#endif
-}
-
-size_t  serialPrintln(const __FlashStringHelper *data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
-}
-
-size_t  serialPrintln(const String &data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
-}
-
-size_t  serialPrintln(const char data[])
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
-}
-
-size_t  serialPrintln(char data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
-}
-
-size_t  serialPrintln(unsigned char data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(int data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(unsigned int data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(long data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(unsigned long data, int format = DEC)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(double data, int format = BIN)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data, format);
-#endif
-}
-
-size_t  serialPrintln(const Printable &data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
-}
-
-size_t  serialPrintln(void)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println();
-#endif
-}
-
-size_t  serialPrintln(const byte data)
-{
-#ifdef SERIAL_DEBUG
-    Serial.println(data);
-#endif
+    Serial.println("* End of Scan\n");
 }
