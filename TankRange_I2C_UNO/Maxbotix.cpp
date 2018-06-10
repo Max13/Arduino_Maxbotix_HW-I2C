ea206c642363832e6bc097fb462be5a0e25dd65f
Adnan RIHAN <adnan@rihan.fr>
2018-06-11 00:28:02 +0100

    Maxbotix is now a class

---
+++ TankRange_I2C_UNO/Maxbotix.cpp
@@ -0,0 +1,75 @@
+/*
+|--------------------------------------------------------------------------
+| Maxbotix Hardware I2C Library
+|--------------------------------------------------------------------------
+|
+| Author: Adnan RIHAN
+| URL: https://github.com/Max13/Maxbotix
+| Licence: CC BY 3.0 (https://creativecommons.org/licenses/by/3.0/)
+|
+*/
+
+#include "Maxbotix.h"
+
+static TwoWire  &Maxbotix::m_i2c = Wire;
+
+Maxbotix::Maxbotix(const byte &i2cAddress, const byte &sonarCmd, bool pullup)
+    : m_address(i2cAddress), m_sonarCmd(sonarCmd)
+{
+    if (pullup) {
+        pinMode(SDA, INPUT_PULLUP);
+        pinMode(SCL, INPUT_PULLUP);
+    }
+
+    m_i2c = Wire;
+    m_i2c.begin();
+}
+
+int             Maxbotix::probe()
+{
+    byte            error = 0;
+    unsigned int    range = 0;
+
+    // Sends command byte and capture error
+    m_i2c.beginTransmission(m_address);
+    m_i2c.write(m_sonarCmd);
+    error = m_i2c.endTransmission();
+
+    // To differenciate a range from an error
+    // probe() will return negative error
+    if (error != 0) {
+        return -error;
+
+    }
+
+    // Wait for the impulse to come back
+    delay(SONAR_WAIT);
+
+    if (m_i2c.requestFrom(int(m_address), 2) == 2) {
+        range = m_i2c.read() * 256;
+        range += m_i2c.read();
+    }
+
+    m_lastProbe = range;
+
+    return range;
+}
+
+unsigned int    Maxbotix::lastProbe() const
+{
+    return m_lastProbe;
+}
+
+const byte      &Maxbotix::findFirstValidAddress(const byte &sonarCmd)
+{
+    for (byte cur = 8; cur <= 127; cur += 2) {
+        Maxbotix::m_i2c.beginTransmission(cur);
+        Maxbotix::m_i2c.write(sonarCmd);
+
+        if (Maxbotix::m_i2c.endTransmission() == 0) {
+            return cur;
+        }
+    }
+
+    return 0;
+}
---
+++ TankRange_I2C_UNO/Maxbotix.h
@@ -0,0 +1,48 @@
+/*
+|--------------------------------------------------------------------------
+| Maxbotix Hardware I2C Library
+|--------------------------------------------------------------------------
+|
+| Author: Adnan RIHAN
+| URL: https://github.com/Max13/Maxbotix
+| Licence: CC BY 3.0 (https://creativecommons.org/licenses/by/3.0/)
+|
+*/
+
+#ifndef MAXBOTIX_H
+#define MAXBOTIX_H
+
+#include    <Arduino.h>
+#include    <Wire.h>
+
+// Delay to wait for the sonar impulse to come back.
+// Minimum recommanded is 100ms
+#ifndef     SONAR_WAIT
+# define    SONAR_WAIT  100
+#endif
+
+// The default I2C address for Maxbotix sonars
+// is 224 on 8 bits, 112 on 7bits.
+// Arduino I2C library currently uses 7bit addresses
+// See: https://www.maxbotix.com/documents/I2CXL-MaxSonar-WR_Datasheet.pdf
+# define    MB_ADDR     byte(112)
+# define    MB_PING_CMD byte(81)
+
+class Maxbotix
+{
+    private:
+        const byte      &m_address;
+        const byte      &m_sonarCmd;
+        static TwoWire  &m_i2c;
+        unsigned int    m_lastProbe;
+
+    public:
+        Maxbotix(const byte &i2cAddress = MB_ADDR, const byte &sonarCmd = MB_PING_CMD, bool pullup = true);
+
+        int                 probe();
+        unsigned int        lastProbe() const;
+
+        static const byte   &findFirstValidAddress(const byte &sonarCmd = MB_PING_CMD);
+};
+
+#endif  // MAXBOTIX_H
